7e8b6eae52381fb11f062f7746eee8660d70e264
Henrik Stanley Mortensen <31671391+HenrikStanley@users.noreply.github.com>
2021-12-30 00:19:03 +0100

    Update awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -1,4 +1,17 @@
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+sudo reflector -c Denmark -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+
 sudo pacman -S xorg-server xorg-xinit xterm awesome
 echo “exec awesome” > ~/.xinitrc
+
+# Options
+aur_helper=true
+
+if [[ $aur_helper = true ]]; then
+    cd /tmp
+    git clone https://aur.archlinux.org/paru.git
+    cd paru/;makepkg -si --noconfirm;cd
+fi
 
 startx
